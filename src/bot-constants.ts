4db6d080ea7d1a638a48269dd2692a35993fdbda
Wannes De Craene <wannesdecraene@hotmail.com>
2021-02-26 15:44:16 +0100

    fix

--- src/bot-constants.ts
+++ src/bot-constants.ts
@@ -2,6 +2,11 @@ export const BotConstants = {
     ROLES: {
         CLOGS_ROLE: 'clogs',
         STEP_LEADER_ROLE: 'Step Leaders'
+    },
+    COMMANDS: {
+        PING: 'ping',
+        QUOTES: 'quotes',
+        STOCK: 'stock',
+        USER: 'user'
     }
-
 }
