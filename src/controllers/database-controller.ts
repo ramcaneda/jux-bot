5c659cd7799ca76767d66b45ceae14147cf36a75
Wannes De Craene <wannesdecraene@hotmail.com>
2021-02-27 18:09:13 +0100

    hotfix heroku

--- src/controllers/database-controller.ts
+++ src/controllers/database-controller.ts
@@ -12,15 +12,27 @@ export class DatabaseController {
 
   public dbConnect() : Client {
 
-    let connection = process.env.DATABASE_URL || {
-      host: 'localhost',
-      user: 'postgres',
-      password: 'root',
-      database: 'jux-bot-local',
-      port: 5432
+
+    let client;
+
+    if(process.env.DATABASE_URL){
+      client = new Client({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false
+        }
+      });
+    } else {
+      let connection = {
+        host: 'localhost',
+        user: 'postgres',
+        password: 'root',
+        database: 'jux-bot-local',
+        port: 5432
+      }
+      client = new Client(connection);
     }
 
-    const client = new Client(connection)
 
     client.connect();
 
@@ -32,7 +44,7 @@ export class DatabaseController {
     try {
       testdb = this.dbConnect();
       const query = fs.readFileSync(path.join(__dirname, '../../sql/create-tables.sql')).toString();
-      await testdb.query(query);
+      await testdb.query(query).catch((err) => {console.log(err)});
       console.log('Table is successfully created');
     } catch (err) {
       console.log(err.stack);
