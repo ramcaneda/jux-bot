08b596aead3cf95422b7eaceaccf49a0e0b51506
Wannes De Craene <wannesdecraene@hotmail.com>
2021-02-27 12:16:29 +0100

    Develop (#6)
    * yes
    
    * some updates (#3)
    
    * Refactor/remove easter eggs (#4)
    
    * some updates
    
    * refactor easter eggs
    
    * add github action workflow (#5)
    
    Co-authored-by: Ram Cañeda <ramcaneda@gmail.com>

--- src/controllers/message-controller.ts
+++ src/controllers/message-controller.ts
@@ -4,22 +4,23 @@ import {TYPES} from "../types";
 import { UserModule } from "../modules/user-info/user-info";
 import { Selector } from "../utils/selector";
 import { BotConstants } from "../bot-constants";
+import { RandomResponses } from "../modules/random-responses/random-responses";
 
 @injectable()
 export class MessageResponder {
 
   private userModule: UserModule;
-  private selector: Selector;
   private prefix: string;
+  private random: RandomResponses
 
   constructor(
     @inject(TYPES.UserModule) userModule: UserModule,
-    @inject(TYPES.Selector) selector: Selector,
-    @inject(TYPES.Prefix) prefix: string
+    @inject(TYPES.Prefix) prefix: string,
+    @inject(TYPES.Prefix) random: RandomResponses
   ) {
     this.userModule = userModule;
-    this.selector = selector;
     this.prefix = prefix;
+    this.random = random;
   }
 
   async handle(message: Message): Promise<Message | Message[]> {
@@ -46,36 +47,10 @@ export class MessageResponder {
       await this.userModule.createProfileMessage(message)
       return message.delete();
     }
-
-
-    //Easter eggs
-
-    if(message.author.id === '190986660895260673' && Math.random() < .01){
-      let replies = ['Oh yes... Daddy Kuro!', //
-       'Kurt!', //
-       'How did you like Avatar: the last airbender, the movie?', //
-       'Hey'//
-      ]
-      return message.channel.send(this.selector.randomMessageSelector(replies));
-    }
-
-    if(message.member?.roles.cache.some(role => role.name === BotConstants.ROLES.CLOGS_ROLE) && Math.random() < .01){
-      let replies = ['clogs, lol', //
-      'ew Dutch people', //
-      'Can I have some cheese', //
-      'Hey'
-     ]
-      return message.channel.send(this.selector.randomMessageSelector(replies));
-    }
-
-    if(message.member?.roles.cache.some(role => role.name === BotConstants.ROLES.STEP_LEADER_ROLE) && Math.random() < .01){
-      let replies = ['What are you doing Step Leader!', //
-      'Don\'t be Rhinkey Dink', //
-      'Just blame Harley', //
-      'Hey'
-     ]
-      return message.channel.send(this.selector.randomMessageSelector(replies));
-    }
+    
+    // Easter Eggs
+    
+    this.random.randomResponses(message);
 
     return Promise.reject();
   }
